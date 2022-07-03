58d949cf2a9840d615069595e517ca4671ac99b1
avishka2k <avishka2k@gmail.com>
2022-07-03 13:16:38 +0530

    update

---
+++ lib/auth.config.dart
@@ -0,0 +1,4 @@
+var remoteServerConfig = {
+  "server": "https://mail-abot.herokuapp.com",
+  "serverKey": "jrF6t1"
+};
--- lib/screens/forgotPassword.dart
+++ lib/screens/forgotPassword.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: camel_case_types, use_key_in_widget_constructors, file_names, use_build_context_synchronously, avoid_print
 import 'dart:math';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:email_auth/email_auth.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -18,13 +18,70 @@ class forgotPassword extends StatefulWidget {
 
 class _forgotPasswordState extends State<forgotPassword> {
   final _emailControllar = TextEditingController();
+  final _veifyControllar = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+
+  EmailAuth emailAuth = EmailAuth(sessionName: "Sample session");
+
+  var remoteServerConfig = {
+    "server": "food-app-dc30a.firebaseapp.com",
+    "serverKey": ""
+  };
+
+  Future veri() async {
+    try {
+      await FirebaseAuth.instance
+          .verifyPasswordResetCode(_emailControllar.text);
+    } on FirebaseAuthException catch (e2) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(e2.toString()),
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the package
+    emailAuth;
+    emailAuth.config(remoteServerConfig);
+  }
+
+  bool submitValid = false;
+
+  void sendOtp() async {
+    var result = await emailAuth.sendOtp(
+      recipientMail: _emailControllar.text,
+    );
+    if (result) {
+      print('OTP Send');
+    } else {
+      print('Not Sent');
+    }
+  }
+
+  void verifyOtp() {
+    var result = emailAuth.validateOtp(
+      recipientMail: _emailControllar.text,
+      userOtp: _veifyControllar.text,
+    );
+    if (result) {
+      print('verifyed');
+    }
+  }
 
   @override
   void dispose() {
     _emailControllar.dispose();
     super.dispose();
   }
+
+  bool isError = false;
 
   Future passwordReset() async {
     showDialog(
@@ -39,21 +96,27 @@ class _forgotPasswordState extends State<forgotPassword> {
           .sendPasswordResetEmail(email: _emailControllar.text.trim());
     } on FirebaseAuthException catch (e) {
       print(e.toString());
+      Navigator.pop(context);
+      setState(() {
+        isError = true;
+      });
       showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              content: Text(e.toString()),
-            );
-          });
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(e.toString()),
+          );
+        },
+      );
     }
-    // Navigator.pop(context);
-    // Navigator.push(
-    //   context,
-    //   MaterialPageRoute(
-    //     builder: (context) => verification(),
-    //   ),
-    // );
+    if (!isError) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => verification(),
+        ),
+      );
+    }
   }
 
   @override
@@ -75,130 +138,135 @@ class _forgotPasswordState extends State<forgotPassword> {
               ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 50.h,
-              horizontal: 25.w,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: HexColor('#F9A84D').withOpacity(0.2),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: IconButton(
-                        icon: Image.asset(
-                          'assets/icons/back.png',
-                          color: HexColor('#DA6317'),
-                        ),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20.h),
-                  child: Text(
-                    'Forgot Password?',
-                    style: TextStyle(
-                      fontSize: 25.sp,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Please Enter Your Email Address To \nRecieve a Verification Code.',
-                  style: TextStyle(
-                    fontSize: 13.sp,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.black,
-                  ),
-                ),
-                Form(
-                  key: _formKey,
-                  child: Column(
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 50.h,
+                horizontal: 25.w,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      SizedBox(height: 50.h),
                       Container(
-                        decoration: BoxDecoration(boxShadow: [
-                          BoxShadow(
-                            blurRadius: 100,
-                            offset: const Offset(0, 20),
-                            color: HexColor('#15BE77').withOpacity(0.1),
-                          )
-                        ]),
-                        child: TextFormField(
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Cant\'t be empty';
-                            } else if (EmailValidator.validate(
-                                    _emailControllar.text) ==
-                                false) {
-                              return 'Invalid Email Address';
-                            }
-                            return null;
-                          },
-                          controller: _emailControllar,
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.white,
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  width: 0.2.w,
-                                  color: Colors.grey.withOpacity(0.5),
-                                ),
-                                borderRadius: BorderRadius.circular(15)),
-                            hintText: 'example@gmail.com',
-                            labelText: 'Email',
-                            labelStyle: const TextStyle(
-                              color: Colors.grey,
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: HexColor('#15BE77'),
-                                ),
-                                borderRadius: BorderRadius.circular(15)),
-                            focusedErrorBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: HexColor('#15BE77'),
-                                ),
-                                borderRadius: BorderRadius.circular(15)),
-                            errorBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  width: 0.2.w,
-                                  color: Colors.red,
-                                ),
-                                borderRadius: BorderRadius.circular(15)),
-                          ),
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: HexColor('#F9A84D').withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                      ),
-                      SizedBox(height: 50.h),
-                      // ignore: deprecated_member_use
-                      RaisedButton(
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            passwordReset();
-                          }
-                        },
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0)),
-                        padding: const EdgeInsets.all(0.0),
-                        child: primatyButton(btnText: 'Submit'),
+                        child: IconButton(
+                          icon: Image.asset(
+                            'assets/icons/back.png',
+                            color: HexColor('#DA6317'),
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
                       ),
                     ],
                   ),
-                )
-              ],
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20.h),
+                    child: Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                        fontSize: 25.sp,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Please Enter Your Email Address To \nRecieve a Verification Code.',
+                    style: TextStyle(
+                      fontSize: 13.sp,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        SizedBox(height: 50.h),
+                        Container(
+                          decoration: BoxDecoration(boxShadow: [
+                            BoxShadow(
+                              blurRadius: 100,
+                              offset: const Offset(0, 20),
+                              color: HexColor('#15BE77').withOpacity(0.1),
+                            )
+                          ]),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Cant\'t be empty';
+                              } else if (EmailValidator.validate(
+                                      _emailControllar.text) ==
+                                  false) {
+                                return 'Invalid Email Address';
+                              }
+                              return null;
+                            },
+                            controller: _emailControllar,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    width: 0.2.w,
+                                    color: Colors.grey.withOpacity(0.5),
+                                  ),
+                                  borderRadius: BorderRadius.circular(15)),
+                              hintText: 'example@gmail.com',
+                              labelText: 'Email',
+                              labelStyle: const TextStyle(
+                                color: Colors.grey,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: HexColor('#15BE77'),
+                                  ),
+                                  borderRadius: BorderRadius.circular(15)),
+                              focusedErrorBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: HexColor('#15BE77'),
+                                  ),
+                                  borderRadius: BorderRadius.circular(15)),
+                              errorBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    width: 0.2.w,
+                                    color: Colors.red,
+                                  ),
+                                  borderRadius: BorderRadius.circular(15)),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 50.h),
+                        // ignore: deprecated_member_use
+                        RaisedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              veri();
+                            }
+                          },
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0)),
+                          padding: const EdgeInsets.all(0.0),
+                          child: primatyButton(btnText: 'Send OTP'),
+                        ),
+                        TextField(
+                          controller: _veifyControllar,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           )
         ],

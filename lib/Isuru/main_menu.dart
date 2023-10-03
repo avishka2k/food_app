eff3de425c8e157b286f341cda9a809a8df6b310
IsuruUSenarathne <isurusenarathne9@gmail.com>
2023-10-03 19:58:42 +0530

    adding new

---
+++ lib/Isuru/main_menu.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class GetStartedPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [ 
+            //SSDSSDSDSD
+
+            SizedBox(height:20 ,),
+      
+            ElevatedButton(
+              onPressed: () {
+                
+              },
+              child: Text('Get sTARTED'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
